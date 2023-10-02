0ab84934b8c9f36e9ae49f4a6978452e3f21d796
srharuno <minigamer255@gmail.com>
2023-10-02 14:18:46 -0300

    mosselin moveset, purird sprite

--- Plugins/v21.1 Hotfixes/Battle bug fixes.rb
+++ Plugins/v21.1 Hotfixes/Battle bug fixes.rb
@@ -1,68 +1,4 @@
 #===============================================================================
-# "v21.1 Hotfixes" plugin
-# This file contains fixes for bugs in Essentials v21.1.
-# These bug fixes are also in the master branch of the GitHub version of
-# Essentials:
-# https://github.com/Maruno17/pokemon-essentials
-#===============================================================================
-
-Essentials::ERROR_TEXT += "[v21.1 Hotfixes 1.0.2]\r\n"
-
-#===============================================================================
-# Fixed Pokédex not showing male/female options for species with gender
-# differences, and showing them for species without.
-#===============================================================================
-class PokemonPokedexInfo_Scene
-  def pbGetAvailableForms
-    ret = []
-    multiple_forms = false
-    gender_differences = (GameData::Species.front_sprite_filename(@species, 0) != GameData::Species.front_sprite_filename(@species, 0, 1))
-    # Find all genders/forms of @species that have been seen
-    GameData::Species.each do |sp|
-      next if sp.species != @species
-      next if sp.form != 0 && (!sp.real_form_name || sp.real_form_name.empty?)
-      next if sp.pokedex_form != sp.form
-      multiple_forms = true if sp.form > 0
-      if sp.single_gendered?
-        real_gender = (sp.gender_ratio == :AlwaysFemale) ? 1 : 0
-        next if !$player.pokedex.seen_form?(@species, real_gender, sp.form) && !Settings::DEX_SHOWS_ALL_FORMS
-        real_gender = 2 if sp.gender_ratio == :Genderless
-        ret.push([sp.form_name, real_gender, sp.form])
-      elsif sp.form == 0 && !gender_differences
-        2.times do |real_gndr|
-          next if !$player.pokedex.seen_form?(@species, real_gndr, sp.form) && !Settings::DEX_SHOWS_ALL_FORMS
-          ret.push([sp.form_name || _INTL("One Form"), 0, sp.form])
-          break
-        end
-      else   # Both male and female
-        2.times do |real_gndr|
-          next if !$player.pokedex.seen_form?(@species, real_gndr, sp.form) && !Settings::DEX_SHOWS_ALL_FORMS
-          ret.push([sp.form_name, real_gndr, sp.form])
-          break if sp.form_name && !sp.form_name.empty?   # Only show 1 entry for each non-0 form
-        end
-      end
-    end
-    # Sort all entries
-    ret.sort! { |a, b| (a[2] == b[2]) ? a[1] <=> b[1] : a[2] <=> b[2] }
-    # Create form names for entries if they don't already exist
-    ret.each do |entry|
-      if entry[0]   # Alternate forms, and form 0 if no gender differences
-        entry[0] = "" if !multiple_forms && !gender_differences
-      else   # Necessarily applies only to form 0
-        case entry[1]
-        when 0 then entry[0] = _INTL("Male")
-        when 1 then entry[0] = _INTL("Female")
-        else
-          entry[0] = (multiple_forms) ? _INTL("One Form") : _INTL("Genderless")
-        end
-      end
-      entry[1] = 0 if entry[1] == 2   # Genderless entries are treated as male
-    end
-    return ret
-  end
-end
-
-#===============================================================================
 # Fixed AI always switching Pokémon due to unusable moves if the Pokémon is
 # asleep or frozen.
 #===============================================================================
@@ -179,52 +115,6 @@ Battle::AI::Handlers::ShouldSwitch.add(:asleep,
 )
 
 #===============================================================================
-# Fixed class PngAnimatedBitmap animating slowly.
-#===============================================================================
-class PngAnimatedBitmap
-  def initialize(dir, filename, hue = 0)
-    @frames       = []
-    @currentFrame = 0
-    @timer_start  = System.uptime
-    panorama = RPG::Cache.load_bitmap(dir, filename, hue)
-    if filename[/^\[(\d+)(?:,(\d+))?\]/]   # Starts with 1 or 2 numbers in brackets
-      # File has a frame count
-      numFrames = $1.to_i
-      duration  = $2.to_i   # In 1/20ths of a second
-      duration  = 5 if duration == 0
-      raise "Invalid frame count in #{filename}" if numFrames <= 0
-      raise "Invalid frame duration in #{filename}" if duration <= 0
-      if panorama.width % numFrames != 0
-        raise "Bitmap's width (#{panorama.width}) is not divisible by frame count: #{filename}"
-      end
-      @frame_duration = duration / 20.0
-      subWidth = panorama.width / numFrames
-      numFrames.times do |i|
-        subBitmap = Bitmap.new(subWidth, panorama.height)
-        subBitmap.blt(0, 0, panorama, Rect.new(subWidth * i, 0, subWidth, panorama.height))
-        @frames.push(subBitmap)
-      end
-      panorama.dispose
-    else
-      @frames = [panorama]
-    end
-  end
-
-  def totalFrames
-    return (@frame_duration * @frames.length * 20).to_i
-  end
-end
-
-#===============================================================================
-# Fixed being unable to replace a NamedEvent.
-#===============================================================================
-class NamedEvent
-  def add(key, proc)
-    @callbacks[key] = proc
-  end
-end
-
-#===============================================================================
 # Fixed Cramorant's form not reverting after coughing up its Gulp Missile.
 #===============================================================================
 class Battle::Battler
@@ -256,21 +146,6 @@ class Battle::Battler
       end
     end    
     __hotfixes__pbEffectsOnMakingHit(move, user, target)
-  end
-end
-
-#===============================================================================
-# Fixed crash when a phone contact tries to call you while you're on a map with
-# no map metadata.
-#===============================================================================
-class Phone
-  module Call
-    module_function
-
-    def can_make?
-      return false if $game_map.metadata&.has_flag?("NoPhoneSignal")
-      return true
-    end
   end
 end
 
